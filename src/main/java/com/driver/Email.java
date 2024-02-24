85ef0c860f76a6838d28bd95478075153472b193
bhushan baddiya <bhushanbaddiya0@gmail.com>
2024-02-24 21:13:43 +0530

    change

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -17,6 +17,46 @@ public class Email {
     public String getPassword() {
         return password;
     }
+    public boolean verifyUppercase(char [] verify){
+          boolean ans = false;
+        for(int i = 0; i < verify.length; i++){
+            char c = verify[i];
+            if(c >= 'A' && c <= 'Z'){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+    public boolean verifyLowercase(char [] verify){
+        boolean ans = false;
+        for(int i = 0; i < verify.length; i++){
+            char c = verify[i];
+            if(c >= 'a' && c <= 'z'){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+    public boolean isPresentDigit(char [] verify){
+        boolean ans = false;
+        for(int i = 0; i < verify.length; i++){
+            char c = verify[i];
+            if(c >= '0' && c <= '9'){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+    public boolean isSpecialChar(char [] verify){
+        boolean ans = true;
+        for(int i = 0; i < verify.length; i++){
+            char c = verify[i];
+            if((isPresentDigit(verify)) || (verifyLowercase(verify) || (verifyUppercase(verify)))){
+                ans = false;
+            }
+        }
+        return ans;
+    }
 
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
@@ -25,5 +65,20 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+         if(this.password == oldPassword){
+             char [] verify = newPassword.toCharArray();
+             if(verify.length >= 8){
+                 if(verifyUppercase(verify)){
+                     if(verifyLowercase(verify)){
+                         if(isPresentDigit(verify)){
+                             if(isSpecialChar(verify)){
+                               this.password = newPassword;
+                             }
+                         }
+                     }
+                 }
+             }
+         }
+
     }
 }
